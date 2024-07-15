42c46c6d0731942fd1c5b4ce2f744f46c0d28913
Udeagu Pascal <pascaludeagu22@gmail.com>
2024-07-15 12:05:45 +0100

    first commit

---
+++ about.php
@@ -0,0 +1,8 @@
+<?php
+    include_once('assets/header.php');
+?>
+<main>
+    <h1>About Page</h1>
+</main>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+    include_once('assets/header.php');
+
+?>
+<main>
+    <h1>Home Page</h1>
+</main>
+</body>
+</html>
+<?php
+    include_once('assets/header.php');
+?>
